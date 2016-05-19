8487efe10f0d2d0729fe21c8facf7d380aaf9f8e
Katie Simmons <annihilatrix@katie.codes>
2016-05-18 18:04:28 -0700

    Added search route

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$/', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^search', views.search, name='search'),
+
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
 from django.template import loader
 from django.http import HttpResponse
 from .models import Collection, Movie
+from utils import generic_search
+from django.shortcuts import render_to_response, redirect
 
 
 def index(request):
@@ -11,3 +13,28 @@ def index(request):
         'movies': movies,
     }
     return HttpResponse(template.render(context, request))
+
+
+def search(request):
+    """ Takes in http request, returns the search_results html
+        with the objects found by the search available to that template.
+    """
+    query = "search-query"
+    # model fields to return
+    model_map = {Movie: ["title", "year", "plot", "release_date", "poster_loc"], Collection: ["title", "description", "creation_date", "movies"]}
+
+    # list of return results
+    returned_objects = []
+
+    for model, fields in model_map.iteritems():
+        returned_objects += generic_search(request, model, fields, query)
+
+    return render_to_response("movies/search_results.html",
+                              {"results": returned_objects,
+                               "search_string": request.GET.get(query, "")
+                               }
+                              )
+
+# def collection(request):
+#     """ Takes in http request, renders the page containing the form to create collections. """
+#
