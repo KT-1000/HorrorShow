f1301ca7e124a24141344f2647f0bb369258fb58
Katie Simmons <annihilatrix@katie.codes>
2016-05-19 18:32:41 -0700

    Did a thing

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search', views.search, name='search'),
-
+    url(r'^movies', views.movies, name='movies'),
+    url(r'^collections', views.collections, name='collections'),
+    url(r'^create_collection', views.create_collection, name='create_collection'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,16 +1,18 @@
 from django.template import loader
 from django.http import HttpResponse
 from .models import Collection, Movie
-from utils import generic_search
 from django.shortcuts import render_to_response, redirect
 
 
 def index(request):
     """ Takes in http request, renders the main page. """
-    movies = Movie.objects.order_by('release_date')
+    recent_movies = Movie.objects.order_by('-release_date')[:5]
+    featured_collection = Collection.objects.order_by('creation_date')[0]
+    # get featured collection(s)
     template = loader.get_template('movies/index.html')
     context = {
-        'movies': movies,
+        'recent_movies': recent_movies,
+        'featured_collection': featured_collection
     }
     return HttpResponse(template.render(context, request))
 
@@ -19,22 +21,34 @@ def search(request):
     """ Takes in http request, returns the search_results html
         with the objects found by the search available to that template.
     """
-    query = "search-query"
-    # model fields to return
-    model_map = {Movie: ["title", "year", "plot", "release_date", "poster_loc"], Collection: ["title", "description", "creation_date", "movies"]}
+    # query = "search-query"
+    # # model fields to return
+    # model_map = {Movie: ["title", "year", "plot", "release_date", "poster_loc"], Collection: ["title", "description", "creation_date", "movies"]}
+    #
+    # return render_to_response("movies/search_results.html")
+    pass
 
-    # list of return results
-    returned_objects = []
 
-    for model, fields in model_map.iteritems():
-        returned_objects += generic_search(request, model, fields, query)
+def movies(request):
+    """ Takes in http request, renders all movies, ordered by release date, paginated. """
+    movies = Movie.objects.order_by('release_date')
+    template = loader.get_template('movies/movies.html')
+    context = {
+        'movies': movies,
+    }
+    return HttpResponse(template.render(context, request))
 
-    return render_to_response("movies/search_results.html",
-                              {"results": returned_objects,
-                               "search_string": request.GET.get(query, "")
-                               }
-                              )
 
-# def collection(request):
-#     """ Takes in http request, renders the page containing the form to create collections. """
-#
+def collections(request):
+    """ Takes in http request, renders all collections, ordered by creation date, paginated. """
+    collections = Collection.objects.order_by('creation_date')
+    template = loader.get_template('movies/collections.html')
+    context = {
+        'collections': collections,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def create_collection(request):
+    """ Takes http request, renders the page containing the form to create collections. """
+    pass
