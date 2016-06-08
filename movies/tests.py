198fdc4046a9536b9a98e3dbb3d00bd10c9f9047
annihilatrix <katie@katie.codes>
2016-06-07 18:55:56 -0700

    Templates looking good, some small styling issues with font colors and link styling but perfectly presentable

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+import unittest
 
-# Create your tests here.
+
+class TestBasic(unittest.TestCase):
+    "Basic tests"
+
+    def test_basic(self):
+        a = 1
+        self.assertEqual(1, a)
+
+    def test_basic_2(self):
+        a = 1
+        assert a == 1
+
+
+class TestBasic2(unittest.TestCase):
+    "Show setup and teardown"
+
+    def setUp(self):
+        self.a = 1
+
+    def tearDown(self):
+        del self.a
+
+    def test_basic1(self):
+        "Basic with setup"
+
+        self.assertNotEqual(self.a, 2)
+
+    def test_basic2(self):
+        "Basic2 with setup"
+        assert self.a != 2
+
+    def test_fail(self):
+        "This test should fail"
+        assert self.a == 2
